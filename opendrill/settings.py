8a199e3819f7d6de6245d64736ff4247a49a5693
Jonathan <jonathancg90@gmail.com>
2014-11-07 11:54:30 -0500

    update settings

--- opendrill/settings.py
+++ opendrill/settings.py
@@ -52,25 +52,25 @@ WSGI_APPLICATION = 'opendrill.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'opendrill',
-#         'USER': 'open',
-#         'PASSWORD': 'Nexonet2014$',
-#         'HOST': '69.164.207.187',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '',                      # Set to empty string for default.
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'opendrill',
+        'USER': 'open',
+        'PASSWORD': 'Nexonet2014$',
+        'HOST': '69.164.207.187',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
 
 
 # DATABASES = {
